0b45154894eb4e75be0883676e2f38a6b3c544ce
SergioGalindo <s.erickgalindo3@gmail.com>
2024-02-29 12:35:18 -0600

    05 Exam, exam module, breackdown

--- exam/models.py
+++ exam/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+from career.models import Career
+from library.models import Module, Question
 class Stage(models.Model):
     stage = models.IntegerField(
             verbose_name ="Etapa",
@@ -13,11 +16,11 @@ class Stage(models.Model):
         months = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio',
         'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre']
         return months[self.application_date.month - 1 ]
-    month.short_description ='Mes'
+    month.short_description = 'Mes'
 
     def year(self):
         return self.application_date.year
-    year.short_description ='Año'
+    year.short_description = 'Año'
 
     def _str_(self):
         return f"{ self.stage } - { self.month() } { self.year() }"
@@ -25,3 +28,25 @@ class Stage(models.Model):
     class Meta:
         verbose_name = "etapa"
         verbose_name_plural = "etapas"
+
+class Exam(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    career = models.ForeignKey(Career, on_delete=models.CASCADE)
+    stage = models.ForeignKey(Stage, on_delete=models.CASCADE)
+    modules = models.ManyToManyField(Module, through='ExamModule')
+    questions = models.ManyToManyField(Question, through='Breakdown')
+    score = models.FloatField(default=0.0)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+class ExamModule(models.Model):
+    exam = models.ForeignKey(Exam, on_delete=models.CASCADE)
+    module = models.ForeignKey(Module, on_delete=models.CASCADE)
+    active = models.BooleanField(default=True)
+    score = models.FloatField(default=0.0)
+
+class Breakdown(models.Model):
+    exam = models.ForeignKey(Exam, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer = models.CharField(max_length=5, default='_')
+    correct = models.CharField(max_length=5, default='_')
